1aee757d3179709185b6671e558e92d7fa39b71d
arpittiwari24 <atofficial2410@gmail.com>
2024-02-16 14:20:34 +0530

    added changes

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import express  from "express";
-import fast2sms from "fast-two-sms"
+import emailRouter from "./routes/sendEmail.js";
+import { config } from "dotenv";
+// import fast2sms from "fast-two-sms"
 const app = express()
 
-async function send  (){
-  var options = {authorization : api_key , message : 'Ram Ram !!' ,  numbers : ['9315165820','9899128212']} 
-console.log(await fast2sms.sendMessage(options))
-}
-send()
+config({
+  path: "./.env"
+})
+app.use(express.json())
+app.use("/",emailRouter)
+
 app.listen(15000,() => {
   console.log("Server running on port 15000");
 })
--- routes/sms.js
+++
@@ -1,5 +0,0 @@
-import express from "express"
-
-const smsRouter = express.Router()
-
-export default smsRouter
